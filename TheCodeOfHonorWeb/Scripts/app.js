c0229cfa7a572f6a608de94f6a8c2445115af21e
Alen Joy <alenjoy333@gmail.com>
2015-06-17 13:43:21 +0530

    Initial Commit

---
+++ TheCodeOfHonorWeb/Scripts/app.js
@@ -0,0 +1,21 @@
+﻿var app = function () {
+    var appMethods = {};
+    appMethods.connection = null;
+
+    appMethods.initialize = function () {
+        appMethods.createConnection();
+        appMethods.bindShootingEvents();
+    };
+
+    appMethods.createConnection = function () {
+        appMethods.connection = $.connection("/CodeOfHonor");
+    };
+
+    appMethods.bindShootingEvents = function () {
+
+    };
+    return appMethods;
+}();
+$(function () {
+    app.initialize();
+});
